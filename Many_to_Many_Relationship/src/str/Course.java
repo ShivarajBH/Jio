0de43c82017886546cd4381f2e7e9d64fc6af008
shivraj <shivraj@TRAINEE-PC.it.com>
2017-04-03 16:48:10 +0530

    Added sop

---
+++ Many_to_Many_Relationship/src/str/Course.java
@@ -0,0 +1,49 @@
+package str;
+
+import java.util.Set;
+
+public class Course {
+
+	private int courseId;
+	private String courseName;
+	private Integer duration;
+	
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	private Set students;
+	
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public Set getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set students) {
+		this.students = students;
+	}	
+	
+}
